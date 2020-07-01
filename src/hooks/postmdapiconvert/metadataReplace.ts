433ce630e7f64ee8fbf071859e62ff149b1cc7f6
Max Camp-Oberhauser <moberhauser@salesforce.com>
2020-07-01 16:15:32 -0600

    Hook updates the source but not the pused source

--- src/hooks/postmdapiconvert/metadataReplace.ts
+++ src/hooks/postmdapiconvert/metadataReplace.ts
@@ -1,15 +1,48 @@
 import { SourceHook } from 'salesforce-alm/dist/lib/source/sourceHooks';
+import { JsonArray, JsonMap } from '@salesforce/ts-types';
+import * as fs from 'fs';
+import { parseString, Builder } from 'xml2js';
 
-export const hook: SourceHook<'postmdapiconvert'> = async function(options) {
-  console.log(
-    `example postmdapiconvert hook running on command ${options.commandId}`
-  );
+export const hook: SourceHook<'postmdapiconvert'> = async function (options) {
 
-  process.stdout.write(
-    `${process.env.NEW_METADATA_VALUE} is the new description\n`
-  );
+  if (options.commandId === 'force:source:push') {
 
-  process.stdout.write(`${options.result} is the result variable\n`);
+    const aggregateSourceElements = <JsonArray>(<JsonArray>options.result)[0];
+
+    if (aggregateSourceElements[0]) {
+
+      (<JsonArray>(<JsonMap>aggregateSourceElements[0])['workspaceElements']).forEach(workspaceElement => {
+
+        let filePath = <string>(<JsonMap>workspaceElement)['sourcePath'];
+
+        if (filePath.includes('objects') && filePath.includes('object-meta.xml')) {
+
+          fs.readFile(filePath, 'utf-8', function (err, data) {
+            if (err) throw err;
+
+            if (data) {
+              parseString(data, function (err, json) {
+                if (err) throw err;
+
+                json.CustomObject.description = process.env.NEW_METADATA_VALUE || "Default new description";
+
+                let xml = new Builder().buildObject(json);
+
+                fs.writeFile(filePath, xml, function () { });
+
+              });
+            }
+
+          });
+
+        }
+
+      });
+
+    }
+
+  }
+
 };
 
 export default hook;
