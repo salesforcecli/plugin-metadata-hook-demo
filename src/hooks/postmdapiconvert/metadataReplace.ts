7d21ea6749a20f21b903ccba4dd3fc07ef5244e3
Max Camp-Oberhauser <moberhauser@salesforce.com>
2020-07-13 13:38:08 -0600

    Deleted default command, updated hook for new postmdapi type

--- src/hooks/postmdapiconvert/metadataReplace.ts
+++ src/hooks/postmdapiconvert/metadataReplace.ts
@@ -1,35 +1,26 @@
 import { SourceHook } from 'salesforce-alm/dist/lib/source/sourceHooks';
-import { JsonArray, JsonMap } from '@salesforce/ts-types';
 import * as fs from 'fs';
 import { Builder, parseString } from 'xml2js';
 
 export const hook: SourceHook<'postmdapiconvert'> = async function (options) {
+  console.log('Hook Running');
+
   // Run only on the push command, not the deploy command
   if (options.commandId === 'force:source:push') {
-    const aggregateSourceElements = <JsonArray>(<JsonArray>options.result)[0];
-    const deployDirectory = <string>(<JsonMap>(<JsonArray>options.result)[2]).deploydir;
 
-    if (aggregateSourceElements[0]) {
-      (<JsonArray>(
-        (<JsonMap>aggregateSourceElements[0])['workspaceElements']
-      )).forEach(workspaceElement => {
-        let filePath = <string>(<JsonMap>workspaceElement)['sourcePath'];
+    if (options.result) {
+      Object.keys(options.result).forEach(mdapiElementName => {
+        console.log('Updating the ' + mdapiElementName + ' object');
+        let mdapiElement = options.result![mdapiElementName]!;
 
-        if (
-          filePath.includes('objects') &&
-          filePath.includes('object-meta.xml')
-        ) {
-          const objectName = <string>(<JsonMap>workspaceElement)['fullName'];
+        // Update the object in the org (the metadata that is being deployed)
+        updateObjectDescription(mdapiElement.mdapiFilePath);
 
-          //Update the description of the object as it is being deployed to the org
-          updateObjectDescription(deployDirectory + '/objects/' + objectName + '.object');
-
-          //Update the description of the object locally
-          updateObjectDescription(filePath);
-
-        }
+        // Update the object locally
+        updateObjectDescription(mdapiElement.workspaceElements[0].sourcePath)
       });
     }
+
   }
 };
 
@@ -43,7 +34,7 @@ function updateObjectDescription(objectPath: string) {
       parseString(data, function (err, json) {
         if (err) throw err;
 
-        // Replace the description of the objects being pushed with the value of an environment variable
+        // Replace the description of the object being pushed with the value of an environment variable
         json.CustomObject.description =
           process.env.NEW_METADATA_VALUE || 'Default new description';
 
