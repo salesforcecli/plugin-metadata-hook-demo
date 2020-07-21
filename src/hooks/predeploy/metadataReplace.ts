ba8b608bdf5940952010eae498adb67d6aaedd9d
Max Camp-Oberhauser <moberhauser@salesforce.com>
2020-07-21 14:22:11 -0600

    File path changed to predeploy

--- src/hooks/predeploy/metadataReplace.ts
+++ src/hooks/predeploy/metadataReplace.ts
@@ -2,7 +2,7 @@ import { SourceHook } from 'salesforce-alm/dist/lib/source/sourceHooks';
 import * as fs from 'fs';
 import { Builder, parseString } from 'xml2js';
 
-export const hook: SourceHook<'predeploy'> = async function (options) {
+export const hook: SourceHook<'predeploy'> = async function(options) {
   console.log('Hook Running');
 
   // Run only on the push command, not the deploy command
@@ -25,11 +25,11 @@ export const hook: SourceHook<'predeploy'> = async function (options) {
 export default hook;
 
 function updateObjectDescription(objectPath: string) {
-  fs.readFile(objectPath, 'utf-8', function (err, data) {
+  fs.readFile(objectPath, 'utf-8', function(err, data) {
     if (err) throw err;
 
     if (data) {
-      parseString(data, function (err, json) {
+      parseString(data, function(err, json) {
         if (err) throw err;
 
         // Replace the description of the object being pushed with the value of an environment variable
@@ -38,7 +38,7 @@ function updateObjectDescription(objectPath: string) {
 
         let xml = new Builder().buildObject(json);
 
-        fs.writeFile(objectPath, xml, function () { });
+        fs.writeFile(objectPath, xml, function() {});
       });
     }
   });
